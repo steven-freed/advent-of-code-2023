202baa68bfd0f82c39b0397700551db1ca025cf8
Steve Freed <freedgitr@gmail.com>
2023-12-06 09:16:13 -0500

    feat: reduce space complexity for day2 part1

--- code/day2.go
+++ code/day2.go
@@ -11,49 +11,25 @@ const (
 	COLON = ":"
 )
 
-type cube struct {
-	red   int
-	green int
-	blue  int
-}
-type games map[int][]cube
-
 func CubeConundrum_Part1(input string, red int, green int, blue int) (int, error) {
-	games := make(games, 0)
+	playableSum := 0
 	for _, line := range strings.Split(input, "\n") {
-		cubeSets := make([]cube, 0)
 		gameNum, err := strconv.Atoi(strings.TrimSpace(line[strings.Index(line, GAME)+len(GAME) : strings.Index(line, COLON)]))
 		if err != nil {
 			return 0, errors.New("unable to convert game number to int")
 		}
+		gamePossible := true
 		cubeSetsStr := strings.Split(strings.TrimSpace(strings.Split(line, COLON)[1]), ";")
 		for _, set := range cubeSetsStr {
-			c := cube{}
 			for _, color := range strings.Split(set, ",") {
 				colorData := strings.Split(strings.TrimSpace(color), " ")
 				num, err := strconv.Atoi(colorData[0])
 				if err != nil {
 					return 0, errors.New("unable to convert cube set number to int")
 				}
-				switch colorData[1] {
-				case "red":
-					c.red = num
-				case "green":
-					c.green = num
-				case "blue":
-					c.blue = num
+				if (colorData[1] == "red" && num > red) || (colorData[1] == "green" && num > green) || colorData[1] == "blue" && num > blue {
+					gamePossible = false
 				}
-			}
-			cubeSets = append(cubeSets, c)
-		}
-		games[gameNum] = cubeSets
-	}
-	playableSum := 0
-	for gameNum, cubeSets := range games {
-		gamePossible := true
-		for _, cube := range cubeSets {
-			if cube.red > red || cube.green > green || cube.blue > blue {
-				gamePossible = false
 			}
 		}
 		if gamePossible {
